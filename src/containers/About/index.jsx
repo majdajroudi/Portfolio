fcc61052b1bb2aa8332741f5c8243b208ac37353
Majd Ajroudi <majd.ajroudi@gmail.com>
2020-11-25 12:11:24 +0300

    add styling to about page

--- src/containers/About/index.jsx
+++ src/containers/About/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import homeImg from "../../images/homeImg.svg"
+import aboutImg from "../../images/majd.svg"
 import { Col, Row} from "antd"
-import {Sides} from "./../../components/Sides"
+import {Sides} from "./../../components/Sides";
 import "./index.css"
 
 const About = () => {
@@ -12,12 +12,13 @@ const About = () => {
                     <Row className="about__contentBox__bottom">
                         <Col className="about__contentBox__textCol">
                             <Col>
+                                <h2>Hi, this is me to the left</h2>
                                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sed enim at ex ultricies sagittis. Phasellus ornare ultrices erat non mollis. Nulla vel cursus est. Integer nec ipsum sodales, ornare ante sit amet, tincidunt nulla. Morbi bibendum ante ante. Donec odio ligula, elementum et sollicitudin vel, consectetur quis arcu. Pellentesque semper tincidunt turpis, quis vulputate leo molestie et.</p> 
                             </Col>
                         </Col>
                         <Col className="about__contentBox__imgCol">
                             <Row className="about__contentBox__imgContainer">
-                                <img src={homeImg} alt="walking man illustration"/>
+                                <img src={aboutImg} alt="walking man illustration"/>
                             </Row>
                         </Col>
                     </Row>
