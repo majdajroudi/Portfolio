d0d71c01758f59a978b5d346288b7f2f7fd04553
Majd Ajroudi <majd.ajroudi@gmail.com>
2020-11-25 10:52:14 +0300

    Add movie project

--- src/containers/Portfolio/index.jsx
+++ src/containers/Portfolio/index.jsx
@@ -4,6 +4,7 @@ import madreamlibsImg from "./../../images/madreamlibs-portfolio.png"
 import juniperImg from "./../../images/juniper-portfolio.png"
 import portfolioImg from "./../../images/portfolio.png"
 import taskManagerImg from "./../../images/task-manager-portfolio.png"
+import movieProjectImg from "./../../images/movie-project-portfolio.png"
 import {Sides} from "./../../components/Sides";
 import {GithubOutlined, LinkOutlined } from "@ant-design/icons";
 import "./index.css";
@@ -12,16 +13,25 @@ const juniperObject = {
     title: "Juniper",
     link: "https://juniper-ist.netlify.app",
     github: "https://github.com/ReCoded-Org/capstone-istanbul-juniper",
-    technologies: ["ReactJS", "Firebase", "i18next", "CSS"],
+    technologies: ["ReactJS", "Firebase", "i18next", "CSS3"],
     description: "An educational gaming platform for kids to teach them aboute climate change and environmental issues",
     image: juniperImg
+}
+
+const movieObject = {
+    title: "Movie Project",
+    link: "https://fadime-ozdemir.github.io/oop-movie/#",
+    github: "https://github.com/fadime-ozdemir/oop-movie",
+    technologies: ["Javascript", "Bootstrap", "HTML5", "CSS3"],
+    description: "A website that displays a database to display famous movies, their ratings and filter movies according to user's choice",
+    image: movieProjectImg
 }
 
 const taskManagerObject = {
     title: "Task Manager",
     link: "https://task-manager-website.netlify.app",
     github: "https://github.com/jennethydyrova/board-project",
-    technologies: ["ReactJS", "Firebase", "Antdesign", "CSS"],
+    technologies: ["ReactJS", "Firebase", "Antdesign", "CSS3"],
     description: "A simple app that lets you create you account and manage your and your team's tasks",
     image: taskManagerImg
 }
@@ -30,7 +40,7 @@ const madreamlibsObject = {
     title: "MadreamLibs",
     link: "https://madreamlibs.netlify.app",
     github: "https://github.com/hiba-machfej/ma-dream-libz",
-    technologies: ["Javascript", "HTML", "CSS"],
+    technologies: ["Javascript", "HTML5", "CSS3"],
     description: "A fun madlibs game for the kids to write their own success story in a fun way",
     image: madreamlibsImg
 }
@@ -39,12 +49,12 @@ const portfolioObject = {
     title: "Portfolio",
     link: "https://majd-ajroudi-portolfio.netlify.app",
     github: "https://github.com/majdajroudi/Portfolio",
-    technologies: ["ReactJS", "Antdesign", "Font Awesome", "CSS"],
+    technologies: ["ReactJS", "Antdesign", "Font Awesome", "CSS3"],
     description: "A personal portfolio of the all the frontend projects developed by Majd Ajroudi",
     image: portfolioImg
 }
 
-const IMAGES = [juniperObject, taskManagerObject,portfolioObject, madreamlibsObject];
+const IMAGES = [juniperObject, movieObject,taskManagerObject,portfolioObject, madreamlibsObject];
 
 const Portfolio = () => {
 
