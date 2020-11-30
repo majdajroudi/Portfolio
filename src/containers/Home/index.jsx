133d28bfd24014ffbd4a90ce4e11eacf0b966659
Majd Ajroudi <majd.ajroudi@gmail.com>
2020-11-30 10:58:06 +0300

    add social media icons

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import homeImg from "../../images/homeImg.svg"
 import {Sides} from "./../../components/Sides"
 // import {motion} from "framer-motion"
+import {GithubOutlined, LinkedinOutlined } from "@ant-design/icons";
 import { Col, Row} from "antd"
 import "./index.css"
 
@@ -17,6 +18,12 @@ const Home = () => {
                             <h1>Majd</h1>
                             <h1>Ajroudi</h1>
                             <h3>Front End Developer</h3>
+                            <Row className="home__contentBox__nameCol__iconsWrapper">
+                                <Row className="home__contentBox__nameCol__icons">
+                                    <a href="https://www.linkedin.com/in/majd-ajroudi/"><LinkedinOutlined /></a>
+                                    <a href="https://github.com/majdajroudi"><GithubOutlined /></a>
+                                </Row>
+                            </Row>
                         </Col>
                     </Col>
                     <Col className="home__contentBox__imgCol">
