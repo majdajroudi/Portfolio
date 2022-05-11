c7ff20d3a819ba733a4a1d5766bc8ee646491f71
majdajroudi <61711692+majdajroudi@users.noreply.github.com>
2022-05-11 17:05:10 +0300

    show drawer button in mobile mode

--- src/components/Drawer/index.jsx
+++ src/components/Drawer/index.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import { Drawer, Button, Row, Col } from 'antd';
-import {NavLink, useLocation} from "react-router-dom";
-import {MenuOutlined } from "@ant-design/icons";
-import {motion} from "framer-motion"
-import {pageTransitions, transitionProperties} from "./../HelperFunctions/index"
+import { NavLink, useLocation } from "react-router-dom";
+import { MenuOutlined } from "@ant-design/icons";
+import { motion } from "framer-motion"
+import { pageTransitions, transitionProperties } from "./../HelperFunctions/index"
 import "./index.css"
 
 const DrawerMenu = () => {
@@ -26,39 +26,31 @@ const DrawerMenu = () => {
     }
   };
 
-  return ( <motion.div
-      initial="initial"
-      animate="in"
-      exit="out"
-      variants={pageTransitions}
-      transition={transitionProperties}
-      key={location.pathname}
-      className="drawerWrapper">
-      <Row className="drawer"
-        style={location.pathname !== "/" && {justifyContent: "space-between"}}>
-        {/*Repeated name on home page looks bad, so only show it when the user is in another route */}
-        {location.pathname !== "/" && <h1 className="drawer__name" >Majd Ajroudi</h1>}
-        <Button className="drawer--button" type="primary" onClick={showDrawer}>
-          <MenuOutlined />
-        </Button>
-        <Drawer
-          title=""
-          placement="right"
-          closable={true}
-          onClose={handleClose}
-          visible={visible}
-          className="drawer__drawer"
-        >
-          <Col className="drawer__drawer__linksWrapper">
-            <NavLink onClick={handleClose} className={isCurrentPath("/")} to="/" > &nbsp;Home &nbsp;</NavLink>
-            <NavLink onClick={handleClose} className={isCurrentPath("/about")} to="/about" >&nbsp;About &nbsp;</NavLink>
-            <NavLink onClick={handleClose} className={isCurrentPath("/projects")} to="/projects" >&nbsp;Projects &nbsp;</NavLink>
-            <NavLink onClick={handleClose} className={isCurrentPath("/blog")} to="/blog">&nbsp;Blog &nbsp;</NavLink>
-            <NavLink onClick={handleClose} className={isCurrentPath("/contact")} to="/contact" >&nbsp;Contact &nbsp;</NavLink>
-          </Col>
-        </Drawer>
-      </Row>
-    </motion.div>
+  return (
+    <Row className="drawer"
+      style={location.pathname !== "/" && { justifyContent: "space-between" }}>
+      {/*only show it when the user is in another route */}
+      {location.pathname !== "/" && <h1 className="drawer__name" >Majd Ajroudi</h1>}
+      <Button className="drawer--button" type="primary" onClick={showDrawer}>
+        <MenuOutlined />
+      </Button>
+      <Drawer
+        title=""
+        placement="right"
+        closable={true}
+        onClose={handleClose}
+        visible={visible}
+        className="drawer__drawer"
+      >
+        <Col className="drawer__drawer__linksWrapper">
+          <NavLink onClick={handleClose} className={isCurrentPath("/")} to="/" > &nbsp;Home &nbsp;</NavLink>
+          <NavLink onClick={handleClose} className={isCurrentPath("/about")} to="/about" >&nbsp;About &nbsp;</NavLink>
+          <NavLink onClick={handleClose} className={isCurrentPath("/projects")} to="/projects" >&nbsp;Projects &nbsp;</NavLink>
+          <NavLink onClick={handleClose} className={isCurrentPath("/blog")} to="/blog">&nbsp;Blog &nbsp;</NavLink>
+          <NavLink onClick={handleClose} className={isCurrentPath("/contact")} to="/contact" >&nbsp;Contact &nbsp;</NavLink>
+        </Col>
+      </Drawer>
+    </Row>
   );
 };
 
